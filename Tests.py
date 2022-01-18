5b788f99b4c56daa1ad3899b8133b17be6840832
Maxim Nyanin <maximnyanin@gamil.com>
2022-01-17 19:43:16 -0500

    Implemented a class for test cases

---
+++ Tests.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    # Player test cases
+    def test_something1(self):
+        self.assertEqual(True, True)
+
+    # Projectile test cases
+    def test_something2(self):
+        self.assertEqual(True, True)
+
+    # Ship test cases
+    def test_something3(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
