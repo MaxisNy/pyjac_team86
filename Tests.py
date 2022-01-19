4cf8ee7065b7546fa386442e75f7d744c22c55b3
Maxim Nyanin <maximnyanin@gmail.com>
2022-01-19 02:06:37 -0500

    Added Player tests

--- Tests.py
+++ Tests.py
@@ -1,19 +1,38 @@
 import unittest
+from Player import Player
 
 
-class MyTestCase(unittest.TestCase):
+class PlayerTests(unittest.TestCase):
+    """
+    Player test cases.
+    """
 
-    # Player test cases
-    def test_something1(self):
-        self.assertEqual(True, True)
+    p = Player(120, 250)
 
-    # Projectile test cases
-    def test_something2(self):
-        self.assertEqual(True, True)
+    def test_player_getters(self):
+        """ tests correctness of getter methods """
+        self.assertEqual(self.p.get_x(), 120)
+        self.assertEqual(self.p.get_y(), 250)
+        self.assertEqual(self.p.get_speed(), self.p.BLOCK_DOWN_SPEED)
 
-    # Ship test cases
-    def test_something3(self):
-        self.assertEqual(True, True)
+    def test_move_left(self):
+        """ tests leftward movement """
+        old_x = self.p.get_x()
+        self.p.move_left()
+        self.assertEqual(self.p.get_x(), old_x - self.p.get_speed())
+
+    def test_move_right(self):
+        """ test rightward movement """
+        old_x = self.p.get_x()
+        self.p.move_right()
+        self.assertEqual(self.p.get_x(), old_x + self.p.get_speed())
+
+    def test_block(self):
+        """ tests defensive mode """
+        self.p.block_up()
+        self.assertEqual(self.p.get_speed(), self.p.BLOCK_UP_SPEED)
+        self.p.block_down()
+        self.assertEqual(self.p.get_speed(), self.p.BLOCK_DOWN_SPEED)
 
 
 if __name__ == '__main__':
