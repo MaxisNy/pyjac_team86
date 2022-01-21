6c0e8ff0e8ebf86fbb017f454c745062fe96b15f
Adam Kanoun <adam.kanoun@gmail.com>
2022-01-21 18:18:18 -0500

    Added new sprites, additional UI, and game instructions

--- Main.py
+++ Main.py
@@ -1,21 +1,9 @@
 from typing import List
-
 import pygame
 import Settings
+from Timer import Timer
 from Ship import Ship, EnemyShip, PlayerShip
 from load_img import load_image
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-DARK_GRAY = (40, 40, 40)
-LIGHT_GRAY = (100, 100, 100)
-ORANGE = (255, 165, 0)
-GREEN = (0, 128, 0)
-RED = (100, 0, 0)
-BLUE = (30, 144, 255)
-DARK_BLUE = (0, 0, 205)
-PURPLE = (102, 0, 204)
-YELLOW = (255, 255, 0)
 
 
 class Screen:
@@ -25,9 +13,14 @@ class Screen:
     SCREEN_WIDTH: int
     SCREEN_HEIGHT: int
     screen: pygame.surface
-    game_running: bool
-    actors: pygame.sprite.Group
     background: pygame.image
+    game_running: bool
+    intro_running: bool
+    running: bool
+    actors: pygame.sprite.Group
+    main_ship: Ship
+    game_timer: Timer
+    best_time: float
 
     def __init__(self) -> None:
         """
@@ -35,7 +28,10 @@ class Screen:
         """
         self.SCREEN_WIDTH = Settings.SCREEN_WIDTH
         self.SCREEN_HEIGHT = Settings.SCREEN_HEIGHT
+        self.running = True
+        self.intro_running = True
         self.game_running = True
+        self.running = True
         pygame.init()
         pygame.display.set_caption("HackathonGame")
         self.screen = \
@@ -43,41 +39,116 @@ class Screen:
         self.actors = pygame.sprite.Group()
         self._initialize_actors()
         self.background = load_image(Settings.BACKGROUND_IMG)
+        self.game_timer = Timer()
+        self.best_time = 0
 
     def _initialize_actors(self) -> None:
         """
         Initializes all the game actors
         """
-        left_ship = EnemyShip()
-        main_ship = PlayerShip()
-        right_ship = EnemyShip(True)
-        distance_from_wall = self.SCREEN_WIDTH / 40
-        enemy_ship_size_x = right_ship.rect.size[0]
-        enemy_ship_size_y = right_ship.rect.size[1]
-        player_ship_size_x = main_ship.rect.size[0]
-        player_ship_size_y = main_ship.rect.size[1]
-        left_ship.rect.x = distance_from_wall
-        left_ship.rect.y = (self.SCREEN_HEIGHT / 2)
-        right_ship.rect.x = (self.SCREEN_WIDTH - distance_from_wall - enemy_ship_size_x)
-        right_ship.rect.y = (self.SCREEN_HEIGHT / 2)
-        main_ship.rect.x = (self.SCREEN_WIDTH / 2) - player_ship_size_x / 2
-        main_ship.rect.y = (self.SCREEN_HEIGHT / 2) - (player_ship_size_y / 6)
-        self.actors.add(left_ship, main_ship, right_ship)
+        ships = Ship.initialize_game_ships()
+        self.main_ship = ships[1]
+        self.actors.add(ships[0], ships[1], ships[2])
+        self.actors.add()
+
+    def draw_background(self) -> None:
+        """
+        Draws the background of the game.
+        """
+        # Genuine background
+        self.screen.blit(self.background, (0, 0))
+        # Background of Main Ship
+        self.screen.blit(
+            PlayerShip.load_ship_background(),
+            (self.main_ship.rect.x, self.main_ship.rect.y -
+             self.main_ship.rect.size[1] / 5))
+
+    def draw_health_bar(self) -> None:
+        """
+        Draws the health bar of the ship.
+        """
+        heart = load_image(Settings.HEART_IMG, self.SCREEN_WIDTH // 30, self.SCREEN_WIDTH // 30)
+        for i in range(0, self.main_ship.get_ship_health()):
+            self.screen.blit(heart, (10 + 55*i, 10))
+
+    def draw_elapsed_time(self) -> None:
+        """
+        Draws the elapsed time since the game started.
+        """
+        current_time = self.game_timer.get_time_elapsed()
+        if current_time > self.best_time:
+            self.best_time = current_time
+        font = pygame.font.Font('freesansbold.ttf', 32)
+        text = font.render(("Time: {0:.1f}".format(
+            current_time)), True, Settings.BLACK)
+        font2 = pygame.font.Font('freesansbold.ttf', 32)
+        text2 = font.render("Best time {0:.1f}".format(self.best_time), True,
+                            Settings.LIGHT_GRAY)
+        text_rect = text.get_rect()
+        text_rect.x = self.SCREEN_WIDTH - text_rect.size[0]
+        text_rect.y = 0
+        text_rect2 = text2.get_rect()
+        text_rect2.x = self.SCREEN_WIDTH - text_rect2.size[0]
+        text_rect2.y = 10 + text_rect.size[1]
+        self.screen.blit(text, text_rect)
+        self.screen.blit(text2, text_rect2)
+
+    def draw_intro_text(self):
+        """
+        Responsible for drawing the text for the introduction.
+        Includes summary of game and instructions
+        """
+        font = pygame.font.Font('freesansbold.ttf', 25)
+        height = self.SCREEN_HEIGHT // 6
+        for text in Settings.intro_texts:
+            temp_text = font.render(text, True, Settings.DARK_GRAY)
+            text_rect1 = temp_text.get_rect()
+            text_rect1.center = (self.SCREEN_WIDTH // 2, height)
+            height += 3 * text_rect1.size[1]
+            self.screen.blit(temp_text, text_rect1)
+
+    def run_intro(self):
+        """
+        Responsible for running the game introduction.
+        """
+        while self.intro_running and self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.intro_running = False
+                    self.running = False
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    self.intro_running = False
+            self.screen.blit(self.background, (0, 0))
+            self.draw_intro_text()
+            pygame.display.update()
 
     def run_game(self):
         """
         Responsible for running the main loop of the game.
         """
-        while self.game_running:
+        self.game_timer.start()
+        while self.game_running and self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.game_running = False
+                    self.running = False
                     pygame.quit()
-            self.screen.blit(self.background, (0, 0))
+            self.draw_background()
             self.actors.draw(self.screen)
+            self.draw_health_bar()
+            self.draw_elapsed_time()
             pygame.display.update()
+
+    def run(self):
+        """
+        Responsible for the entire game loop.
+        """
+        while self.running:
+            self.run_intro()
+            self.run_game()
 
 
 if __name__ == '__main__':
     screen = Screen()
-    screen.run_game()
+    screen.run()
--- Ship.py
+++ Ship.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from typing import Tuple
+from load_img import load_image
 import pygame
 import Settings
 
@@ -20,6 +23,27 @@ class Ship(pygame.sprite.Sprite):
         """
         raise NotImplementedError
 
+    @ staticmethod
+    def initialize_game_ships() -> Tuple[EnemyShip, PlayerShip, EnemyShip]:
+        """
+        Initializes all the game ships.
+        """
+        left_ship = EnemyShip()
+        main_ship = PlayerShip()
+        right_ship = EnemyShip(True)
+        distance_from_wall = Settings.SCREEN_WIDTH / 40
+        enemy_ship_size_x = right_ship.rect.size[0]
+        player_ship_size_x = main_ship.rect.size[0]
+        player_ship_size_y = main_ship.rect.size[1]
+        left_ship.rect.x = distance_from_wall
+        left_ship.rect.y = (Settings.SCREEN_HEIGHT / 2)
+        right_ship.rect.x = \
+            (Settings.SCREEN_WIDTH - distance_from_wall - enemy_ship_size_x)
+        right_ship.rect.y = (Settings.SCREEN_HEIGHT / 2)
+        main_ship.rect.x = (Settings.SCREEN_WIDTH / 2) - player_ship_size_x / 2
+        main_ship.rect.y = (Settings.SCREEN_HEIGHT / 2) - (player_ship_size_y / 4)
+        return left_ship, main_ship, right_ship
+
 
 class EnemyShip(Ship):
     """
@@ -35,8 +59,7 @@ class EnemyShip(Ship):
         """
         Gets the image of the ship.
         """
-        img = pygame.image.load(Settings.ENEMY_SHIP_IMG).convert_alpha()
-        img = pygame.transform.scale(img, (Settings.SCREEN_WIDTH // 7, Settings.SCREEN_WIDTH // 7))
+        img = load_image(Settings.ENEMY_SHIP_IMG, (int(Settings.SCREEN_WIDTH / 6.3)), int(Settings.SCREEN_WIDTH / 6.3))
         return pygame.transform.flip(img, flipped, False)
 
 
@@ -49,12 +72,41 @@ class PlayerShip(Ship):
         Create an instance of a player ship.
         """
         super().__init__()
-        self.ship_health = 100
+        self.ship_health = 5
 
     def load_ship_sprite(self, flipped: bool = False):
         """
         Gets the image of the ship.
         """
-        img = pygame.image.load(Settings.PLAYER_SHIP_IMG).convert_alpha()
-        img = pygame.transform.scale(img, (int(2 * Settings.SCREEN_WIDTH // 3.6), int(Settings.SCREEN_WIDTH // 3.6)))
+        img = load_image(Settings.PLAYER_SHIP_IMG, int(2 * Settings.SCREEN_WIDTH // 4), int(Settings.SCREEN_WIDTH // 4))
         return pygame.transform.flip(img, flipped, False)
+
+    @staticmethod
+    def load_ship_background():
+        """
+        Loads the background image of the ship
+        """
+        return load_image(Settings.PLAYER_SHIP_BG_IMG,
+                          int(2 * Settings.SCREEN_WIDTH // 4),
+                          int(Settings.SCREEN_WIDTH // 4))
+
+    def damage(self, sprite: pygame.Sprite.sprite) -> None:
+        """
+        Lowers ship_health if the ship is damaged.
+        """
+        if self.is_hit(sprite):
+            self.ship_health -= 1
+
+    def is_hit(self, sprite: pygame.Sprite.sprite) -> bool:
+        """
+        Returns whether the ship has been hit
+        """
+        if self.rect.colliderect(sprite.rect):
+            return True
+        return False
+
+    def get_ship_health(self) -> int:
+        """
+        Returns the current ship_health
+        """
+        return self.ship_health
