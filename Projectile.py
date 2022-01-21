779bf09dd434cc6b13073724c49aa7014e5c5871
Maxim Nyanin <maximnyanin@gmail.com>
2022-01-21 00:05:00 -0500

    Implemented random velocity generation

--- Projectile.py
+++ Projectile.py
@@ -1,11 +1,11 @@
 from Timer import Timer
 import pygame
+import random
 
 
 class Projectile:
 
     INITIAL_COORDINATES = (780, 600)
-    VELOCITY = 100
     ACCELERATION = 50
 
     WIDTH = 20
@@ -14,23 +14,25 @@ class Projectile:
     def __init__(self, side):
         self.x_0 = self.get_init_coords(side)
         self.y_0 = 600
-        self.timer = Timer()
+        # TODO: adjust the range of velocities
+        self.velocity = random.randint(100, 150)
+        self._timer = Timer()
         self.launched = False
         self.side = side
 
     def get_init_coords(self, side):
         if side:
-            return 1000
-        return 300
+            return 1200
+        return 80
 
     def get_x(self, side):
-        return self.x_0 + ((-1)**side) * self.VELOCITY * \
-               self.timer.get_time_elapsed()
+        return self.x_0 + ((-1)**side) * self.velocity * \
+               self._timer.get_time_elapsed()
 
     def get_y(self):
         return self.y_0 - \
-               self.VELOCITY * self.timer.get_time_elapsed() + \
-               (self.ACCELERATION * (self.timer.get_time_elapsed()**2) / 2)
+               self.velocity * self._timer.get_time_elapsed() + \
+               (self.ACCELERATION * (self._timer.get_time_elapsed() ** 2) / 2)
 
     def draw(self, screen):
         """
@@ -43,7 +45,4 @@ class Projectile:
     def launch(self):
         if not self.launched:
             self.launched = True
-            self.timer.start()
-
-
-
+            self._timer.start()
