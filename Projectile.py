9aa0eaa6c022c9e6b40a3d96bed9f0197867cbad
Maxim Nyanin <maximnyanin@gamil.com>
2022-01-21 20:19:11 -0500

    Allowed drawing projectile sprites

--- Projectile.py
+++ Projectile.py
@@ -1,18 +1,10 @@
+import Settings
 from Timer import Timer
+from load_img import load_image
 import pygame
 import random
 import math
 import os
-
-
-def load_image(img_name: str, width: int = 1280,
-               height: int = 720) -> pygame.image:
-    """
-    Return a pygame img of the PNG img_name that has been scaled according
-    to the given width and size
-    """
-    img = pygame.image.load(img_name).convert_alpha()
-    return pygame.transform.scale(img, (width, height))
 
 
 class Projectile(pygame.sprite.Sprite):
@@ -27,15 +19,17 @@ class Projectile(pygame.sprite.Sprite):
 
     def __init__(self, side):
         super().__init__()
-        self.x_0 = self.get_init_coords(side)
-        self.y_0 = 600
         # TODO: adjust the range of velocities
         self.velocity = random.randint(150, 200)
         self._timer = Timer()
         self.launched = False
         self.side = side
-        self.image = load_image(os.path.join("sprites", "projectile.png"))
+        self.image = load_image(
+            Settings.PROJECTILE_IMG, Settings.SCREEN_WIDTH // 35,
+            Settings.SCREEN_WIDTH // 35)
         self.rect = self.image.get_rect()
+        self.rect.x = self.get_init_coords(side)
+        self.rect.y = 600
 
     def get_init_coords(self, side):
         if side:
@@ -43,12 +37,12 @@ class Projectile(pygame.sprite.Sprite):
         return 80
 
     def get_x(self, side):
-        return self.x_0 + ((-1)**side) * self.velocity * \
+        return self.rect.x + ((-1)**side) * self.velocity * \
                math.cos(self.ANGLE * math.pi / 180) * \
                self._timer.get_time_elapsed()
 
     def get_y(self):
-        return self.y_0 - \
+        return self.rect.y - \
                self.velocity * math.sin(self.ANGLE * math.pi / 180) * \
                self._timer.get_time_elapsed() + \
                (self.ACCELERATION * (self._timer.get_time_elapsed() ** 2) / 2)
@@ -57,9 +51,7 @@ class Projectile(pygame.sprite.Sprite):
         """
         Draws the Player avatar on the screen.
         """
-        # TODO: implement image drawing
-        pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(
-            self.get_x(self.side), self.get_y(), self.WIDTH, self.HEIGHT))
+        screen.blit(self.image, (self.get_x(self.side), self.get_y()))
 
     def launch(self):
         if not self.launched:
